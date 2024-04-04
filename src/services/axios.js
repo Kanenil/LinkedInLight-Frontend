076b4dba99f1246550588242cb6d717e13ecc881
Oleksandr Burda <oleksandrburda2004@gmail.com>
2024-04-04 12:13:55 +0300

    Fix default setup of Bearer token in axios.js

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,5 +1,8 @@
 import Axios from 'axios';
 import {APP_ENV} from "../env";
+import {general} from "../constants/general";
+
+const token = localStorage.getItem(general.token);
 
 const axios = Axios.create({
     baseURL: APP_ENV.REMOTE_HOST_NAME,
@@ -8,5 +11,8 @@ const axios = Axios.create({
         "Access-Control-Allow-Origin": "*"
     }
 });
+
+axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
 
 export default axios;
