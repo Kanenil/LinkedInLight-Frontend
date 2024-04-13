42ea392d29ee1a66627ab359aeaca5b6e0a2245a
Yaroslavhal <yaroslav.halushko.n@gmail.com>
2024-04-12 23:08:15 -0400

    password change final fix

--- src/components/profile/settings/blocks/PasswordChange/PasswordChange.js
+++ src/components/profile/settings/blocks/PasswordChange/PasswordChange.js
@@ -11,6 +11,18 @@ const PasswordChange = () => {
     new: false,
     secure: false,
   });
+  const [currentPassword, setCurrentPassword] = useState("");
+  const handleCurrentPasswordChange = (event) => {
+    setCurrentPassword(event.target.value);
+  };
+  const [newPassword, setNewPassword] = useState("");
+  const handleNewPasswordChange = (event) => {
+    setNewPassword(event.target.value);
+  };
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const handleConfirmPasswordChange = (event) => {
+    setConfirmPassword(event.target.value);
+  };
   const handleClick = (value) => {
     setVisibility((prevVisibility) => ({
       ...prevVisibility,
@@ -32,6 +44,8 @@ const PasswordChange = () => {
       <div className="mt-2">Enter current password*</div>
       <div className="relative w-1/2">
         <input
+          value={currentPassword}
+          onChange={handleCurrentPasswordChange}
           type={visibility.current ? "text" : "password"}
           className="mt-2 rounded-2xl w-full"
           placeholder="Current password"
@@ -52,6 +66,8 @@ const PasswordChange = () => {
       <div className="mt-2">Enter new password*</div>
       <div className="relative w-1/2">
         <input
+          value={newPassword}
+          onChange={handleNewPasswordChange}
           type={visibility.new ? "text" : "password"}
           className="mt-2 rounded-2xl w-full"
           placeholder="New password"
@@ -71,6 +87,8 @@ const PasswordChange = () => {
       <div className="mt-2">Confirm new password*</div>
       <div className="relative w-1/2">
         <input
+          value={confirmPassword}
+          onChange={handleConfirmPasswordChange}
           type={visibility.confirm ? "text" : "password"}
           className="mt-2 rounded-2xl w-full"
           placeholder="Confirm new password"
@@ -93,7 +111,15 @@ const PasswordChange = () => {
           Require to sign in with new password in all devices
         </div>
       </div>
-      <div className="mb-8 text-lg text-white text-center bg-indigo-800 hover:bg-indigo-900 hover:text-gray-200 w-[200px] rounded-full py-2 font-semibold">
+      <div
+        className={`mb-8 text-lg w-[200px] rounded-full py-2 font-semibold ${
+          currentPassword.length > 0 &&
+          newPassword.length > 0 &&
+          confirmPassword.length > 0
+            ? "text-white text-center bg-indigo-800 hover:bg-indigo-900 hover:text-gray-200"
+            : "text-gray-400 text-center bg-indigo-100"
+        }`}
+      >
         Save Password
       </div>
       <div className="mb-5 mx-4 text-lg text-gray-500 font-semibold hover:text-gray-700 inline-block">
