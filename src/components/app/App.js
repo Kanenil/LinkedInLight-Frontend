e3b73fc0252b0260e4bf9b6a7d2ca1b972b04aff
Oleksandr Burda <oleksandrburda2004@gmail.com>
2024-04-27 18:36:49 +0300

    Added not found page

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,112 +1,133 @@
-import {BrowserRouter, Navigate, Outlet, Route, Routes} from "react-router-dom";
-import SignUp from "../../pages/auth/SignUp";
-import SignIn from "../../pages/auth/SignIn";
-import Profile from "../../pages/profile";
-import Home from "../../pages/home";
-import Layout from "../shared/layouts/Layout";
-import Chats from "../../pages/chat/Chats";
-import AuthLayout from "../shared/layouts/AuthLayout";
-import ConfirmEmail from "../../pages/auth/ConfirmEmail";
-import InLayout from "../shared/layouts/InLayout";
-import {HelmetProvider} from "react-helmet-async";
-import Settings from "../../pages/profile/settings";
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import ChatsLayout from "../shared/layouts/ChatsLayout";
-import React from "react";
-import ImageCropProvider from "../../providers/ImageCropProvider";
-import MyNetwork from "../../pages/network";
-import Connections from "../../pages/network/connections";
-import ChatProvider from "../../providers/ChatProvider";
-import CreateCompany from "../../pages/company/createCompany";
-import AlertProvider from "../../providers/AlertProvider";
-import Alert from "../shared/Alert";
-import Auth from "../../pages/auth";
-import CompanyPage from "../../pages/company";
-import CompanySettingsIndex from "../company/settings/CompanySettingsIndex";
-import AuthRedirect from "../shared/AuthRedirect";
-import CompanyEditIndex from "../company/edit/CompanyEditIndex";
+import {
+	BrowserRouter,
+	Navigate,
+	Outlet,
+	Route,
+	Routes,
+} from "react-router-dom"
+import SignUp from "../../pages/auth/SignUp"
+import SignIn from "../../pages/auth/SignIn"
+import Profile from "../../pages/profile"
+import Home from "../../pages/home"
+import Layout from "../shared/layouts/Layout"
+import Chats from "../../pages/chat/Chats"
+import AuthLayout from "../shared/layouts/AuthLayout"
+import ConfirmEmail from "../../pages/auth/ConfirmEmail"
+import InLayout from "../shared/layouts/InLayout"
+import { HelmetProvider } from "react-helmet-async"
+import Settings from "../../pages/profile/settings"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import ChatsLayout from "../shared/layouts/ChatsLayout"
+import React from "react"
+import ImageCropProvider from "../../providers/ImageCropProvider"
+import MyNetwork from "../../pages/network"
+import Connections from "../../pages/network/connections"
+import ChatProvider from "../../providers/ChatProvider"
+import CreateCompany from "../../pages/company/createCompany"
+import AlertProvider from "../../providers/AlertProvider"
+import Alert from "../shared/Alert"
+import Auth from "../../pages/auth"
+import CompanyPage from "../../pages/company"
+import CompanySettingsIndex from "../company/settings/CompanySettingsIndex"
+import AuthRedirect from "../shared/AuthRedirect"
+import CompanyEditIndex from "../company/edit/CompanyEditIndex"
+import NotFound from "../../pages/404/NotFound"
 
 const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            refetchOnWindowFocus: false,
-        },
-    },
-});
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+		},
+	},
+})
 
 const App = () => {
-    return (
-        <div className="App">
-            <HelmetProvider>
-                <QueryClientProvider client={queryClient}>
-                    <AlertProvider>
-                        <ImageCropProvider>
-                            <ChatProvider>
-                                    <BrowserRouter>
-                                        <Routes>
-                                            <Route path="/" element={<Layout/>}>
-                                                <Route index element={<Home/>}/>
-                                            </Route>
+	return (
+		<div className='App'>
+			<HelmetProvider>
+				<QueryClientProvider client={queryClient}>
+					<AlertProvider>
+						<ImageCropProvider>
+							<ChatProvider>
+								<BrowserRouter>
+									<Routes>
+										<Route path='/' element={<Layout />}>
+											<Route index element={<Home />} />
+										</Route>
 
-                                            <Route path="giveRecommendation/:userId" element={<AuthRedirect  to="/j4y/:user/details/recommendations"/>}/>
+										<Route
+											path='giveRecommendation/:userId'
+											element={
+												<AuthRedirect to='/j4y/:user/details/recommendations' />
+											}
+										/>
 
-                                            <Route path="/j4y">
-                                                <Route index element={<InLayout/>}/>
+										<Route path='/j4y'>
+											<Route index element={<InLayout />} />
 
-                                                <Route element={<InLayout/>}>
-                                                    <Route path=":profileURL" element={<Profile/>}>
-                                                        <Route path="edit/:blockId" element={<Profile/>}>
-                                                            <Route path=":id" element={<Profile/>}/>
-                                                        </Route>
-                                                        <Route path="details/:blockId" element={<Profile/>}>
-                                                            <Route path="edit/:blockId" element={<Profile/>}>
-                                                                <Route path=":id" element={<Profile/>}/>
-                                                            </Route>
-                                                        </Route>
-                                                    </Route>
+											<Route element={<InLayout />}>
+												<Route path=':profileURL' element={<Profile />}>
+													<Route path='edit/:blockId' element={<Profile />}>
+														<Route path=':id' element={<Profile />} />
+													</Route>
+													<Route path='details/:blockId' element={<Profile />}>
+														<Route path='edit/:blockId' element={<Profile />}>
+															<Route path=':id' element={<Profile />} />
+														</Route>
+													</Route>
+												</Route>
 
-                                                    <Route path="my-network" element={<MyNetwork/>}/>
-                                                    <Route path="my-network/connections" element={<Connections/>}/>
-                                                </Route>
+												<Route path='my-network' element={<MyNetwork />} />
+												<Route
+													path='my-network/connections'
+													element={<Connections />}
+												/>
 
-                                                <Route path="chats" element={<ChatsLayout/>}>
-                                                    <Route index element={<Chats/>}/>
-                                                </Route>
+												<Route path='*' element={<Navigate to='not-found' />} />
+												<Route path='not-found' element={<NotFound />} />
+											</Route>
 
-                                                <Route path="company" element={<ChatsLayout/>}>
-                                                    <Route path=":companyId">
-                                                        <Route index element={<CompanyPage/>}/>
-                                                        <Route path="settings" element={<CompanySettingsIndex/>}/>
-                                                        <Route path="edit" element={<CompanyEditIndex/>}/>
-                                                    </Route>
-                                                    <Route path="new" element={<CreateCompany/>}/>
-                                                </Route>
+											<Route path='chats' element={<ChatsLayout />}>
+												<Route index element={<Chats />} />
+											</Route>
 
-                                                <Route path="settings" element={<ChatsLayout/>}>
-                                                    <Route
-                                                        path=":section?/:block?"
-                                                        element={<Settings/>}
-                                                    />
-                                                </Route>
-                                            </Route>
+											<Route path='company' element={<ChatsLayout />}>
+												<Route path=':companyId'>
+													<Route index element={<CompanyPage />} />
+													<Route
+														path='settings'
+														element={<CompanySettingsIndex />}
+													/>
+													<Route path='edit' element={<CompanyEditIndex />} />
+												</Route>
+												<Route path='new' element={<CreateCompany />} />
+											</Route>
 
-                                            <Route path="/auth" element={<AuthLayout/>}>
-                                                <Route index element={<Auth/>}/>
-                                                <Route path="sign-up" element={<SignUp/>}/>
-                                                <Route path="sign-in" element={<SignIn/>}/>
-                                                <Route path="confirm-email" element={<ConfirmEmail/>}/>
-                                            </Route>
-                                        </Routes>
-                                    </BrowserRouter>
-                                <Alert/>
-                                {/*<ReactQueryDevtools buttonPosition="bottom-right"/>*/}
-                            </ChatProvider>
-                        </ImageCropProvider>
-                    </AlertProvider>
-                </QueryClientProvider>
-            </HelmetProvider>
-        </div>
-    );
-};
+											<Route path='settings' element={<ChatsLayout />}>
+												<Route
+													path=':section?/:block?'
+													element={<Settings />}
+												/>
+											</Route>
+										</Route>
 
-export default App;
+										<Route path='/auth' element={<AuthLayout />}>
+											<Route index element={<Auth />} />
+											<Route path='sign-up' element={<SignUp />} />
+											<Route path='sign-in' element={<SignIn />} />
+											<Route path='confirm-email' element={<ConfirmEmail />} />
+										</Route>
+									</Routes>
+								</BrowserRouter>
+								<Alert />
+								{/*<ReactQueryDevtools buttonPosition="bottom-right"/>*/}
+							</ChatProvider>
+						</ImageCropProvider>
+					</AlertProvider>
+				</QueryClientProvider>
+			</HelmetProvider>
+		</div>
+	)
+}
+
+export default App
--- src/components/shared/headers/InHeader.js
+++ src/components/shared/headers/InHeader.js
@@ -1,137 +1,223 @@
-import Logo from "../../../elements/shared/Logo";
-import {Link} from "react-router-dom";
-import BellIcon from "../../../elements/icons/BellIcon";
-import HomeIcon from "../../../elements/icons/HomeIcon";
-import JobsIcon from "../../../elements/icons/JobsIcon";
-import GroupsIcon from "../../../elements/icons/GroupsIcon";
-import MessagesIcon from "../../../elements/icons/MessagesIcon";
-import AccountButton from "../../../elements/buttons/AccountButton";
-import {useQueries} from "@tanstack/react-query";
-import React from "react";
-import {headerQuery} from "../../../constants/combinedQueries";
-import SearchInput from "./SearchInput";
-import HeaderModal from "./HeaderModal";
-import HeaderProvider, {useHeaderContext} from "../../../providers/HeaderProvider";
-import CompanyButton from "../../../elements/buttons/CompanyButton";
-import useMobileDetector from "../../../hooks/useMobileDetector";
-import Show from "../../../elements/shared/Show";
-import classNames from "classnames";
-import useValidateConnectionEvents from "../../../hooks/useValidateConnectionEvents";
+import Logo from "../../../elements/shared/Logo"
+import { Link, NavLink } from "react-router-dom"
+import BellIcon from "../../../elements/icons/BellIcon"
+import HomeIcon from "../../../elements/icons/HomeIcon"
+import JobsIcon from "../../../elements/icons/JobsIcon"
+import GroupsIcon from "../../../elements/icons/GroupsIcon"
+import MessagesIcon from "../../../elements/icons/MessagesIcon"
+import AccountButton from "../../../elements/buttons/AccountButton"
+import { useQueries } from "@tanstack/react-query"
+import React from "react"
+import { headerQuery } from "../../../constants/combinedQueries"
+import SearchInput from "./SearchInput"
+import HeaderModal from "./HeaderModal"
+import HeaderProvider, {
+	useHeaderContext,
+} from "../../../providers/HeaderProvider"
+import CompanyButton from "../../../elements/buttons/CompanyButton"
+import useMobileDetector from "../../../hooks/useMobileDetector"
+import Show from "../../../elements/shared/Show"
+import classNames from "classnames"
+import useValidateConnectionEvents from "../../../hooks/useValidateConnectionEvents"
 
-const MobileHeader = ({pendingRequests, unReadMessages}) => {
-    const {isComponentVisible: searchEnabled, modal} = useHeaderContext();
+const MobileHeader = ({ pendingRequests, unReadMessages }) => {
+	const { isComponentVisible: searchEnabled, modal } = useHeaderContext()
 
-    return (
-        <>
-            <header
-                className={classNames("flex flex-row gap-3 px-5 pt-7 pb-4", {
-                    "absolute top-0 left-0 z-40 w-full sticky bg-white": searchEnabled && modal === 'search'
-                })}
-                style={{boxShadow: "0px 2px 6px 0px #24459A33"}}
-            >
-                <SearchInput/>
+	return (
+		<>
+			<header
+				className={classNames("flex flex-row gap-3 px-5 pt-7 pb-4", {
+					"absolute top-0 left-0 z-40 w-full bg-white":
+						searchEnabled && modal === "search",
+				})}
+				style={{ boxShadow: "0px 2px 6px 0px #24459A33" }}
+			>
+				<SearchInput />
 
-                <div className="flex flex-row">
-                    <CompanyButton/>
+				<div className='flex flex-row'>
+					<CompanyButton />
 
-                    <AccountButton/>
-                </div>
-            </header>
-            <nav
-                className="fixed flex w-full z-20 flex-row justify-center p-5 gap-10 bottom-0 bg-white"
-                style={{boxShadow: "0px 2px 6px 0px #24459A33"}}
-            >
-                <Link to='/j4y' className="p-2">
-                    <HomeIcon className="fill-[#2D2A33] h-5"/>
-                </Link>
+					<AccountButton />
+				</div>
+			</header>
+			<nav
+				className='fixed flex w-full z-20 flex-row justify-center p-5 gap-10 bottom-0 bg-white'
+				style={{ boxShadow: "0px 2px 6px 0px #24459A33" }}
+			>
+				<NavLink
+					end
+					to='/j4y'
+					className={({ isActive }) =>
+						classNames("p-2", {
+							"border-b-2 border-b-[#24459A]": isActive,
+						})
+					}
+				>
+					<HomeIcon className='fill-[#2D2A33] h-5' />
+				</NavLink>
 
-                <Link to='/j4y/my-network' className="p-2">
-                    <GroupsIcon number={pendingRequests?.length > 0 && pendingRequests.length}/>
-                </Link>
+				<NavLink
+					to='/j4y/my-network'
+					className={({ isActive }) =>
+						classNames("p-2", {
+							"border-b-2 border-b-[#24459A]": isActive,
+						})
+					}
+				>
+					<GroupsIcon
+						number={pendingRequests?.length > 0 && pendingRequests.length}
+					/>
+				</NavLink>
 
-                <Link to='/j4y/chats' className="p-2">
-                    <MessagesIcon number={unReadMessages || null}/>
-                </Link>
+				<NavLink
+					to='/j4y/chats'
+					className={({ isActive }) =>
+						classNames("p-2", {
+							"border-b-2 border-b-[#24459A]": isActive,
+						})
+					}
+				>
+					<MessagesIcon number={unReadMessages || null} />
+				</NavLink>
 
-                <Link to='/j4y' className="p-2">
-                    <JobsIcon/>
-                </Link>
+				<NavLink
+					to='/j4y/jobs'
+					className={({ isActive }) =>
+						classNames("p-2", {
+							"border-b-2 border-b-[#24459A]": isActive,
+						})
+					}
+				>
+					<JobsIcon />
+				</NavLink>
 
-                <Link to='/j4y' className="p-2">
-                    <BellIcon/>
-                </Link>
-            </nav>
-        </>
-    )
+				<NavLink
+					to='/j4y/notifications'
+					className={({ isActive }) =>
+						classNames("p-2", {
+							"border-b-2 border-b-[#24459A]": isActive,
+						})
+					}
+				>
+					<BellIcon />
+				</NavLink>
+			</nav>
+		</>
+	)
 }
 
 const InHeader = () => {
-    const {unReadMessages, pendingRequests} = useQueries({
-        queries: headerQuery().map((value) => ({
-            ...value
-        })),
-        combine: (results) => {
-            return {
-                unReadMessages: results[0].data ?? 0,
-                pendingRequests: results[1].data ?? 0
-            }
-        },
-    });
+	const { unReadMessages, pendingRequests } = useQueries({
+		queries: headerQuery().map(value => ({
+			...value,
+		})),
+		combine: results => {
+			return {
+				unReadMessages: results[0].data ?? 0,
+				pendingRequests: results[1].data ?? 0,
+			}
+		},
+	})
 
-    const {isMobile} = useMobileDetector();
+	const { isMobile } = useMobileDetector()
 
-    useValidateConnectionEvents();
+	useValidateConnectionEvents()
 
-    return (
-        <HeaderProvider>
-            <HeaderModal/>
-            <Show>
-                <Show.When isTrue={!isMobile}>
-                    <header className="absolute top-0 left-0 z-40 w-full sticky"
-                            style={{boxShadow: "0px 2px 6px 0px #24459A33"}}>
-                        <div className="bg-white flex flex-row justify-center py-2.5">
-                            <Link to="/j4y">
-                                <Logo className="fill-[#2D2A33] h-10"/>
-                            </Link>
+	return (
+		<HeaderProvider>
+			<HeaderModal />
+			<Show>
+				<Show.When isTrue={!isMobile}>
+					<header
+						className='sticky top-0 left-0 z-40 w-full'
+						style={{ boxShadow: "0px 2px 6px 0px #24459A33" }}
+					>
+						<div className='bg-white flex flex-row justify-center py-2.5'>
+							<Link to='/j4y'>
+								<Logo className='fill-[#2D2A33] h-10' />
+							</Link>
 
-                            <SearchInput/>
+							<SearchInput />
 
-                            <div className="flex flex-row justify-end w-[440px] gap-4">
-                                <Link to='/j4y' className="p-2">
-                                    <HomeIcon/>
-                                </Link>
+							<div className='flex flex-row justify-end w-[440px] gap-4'>
+								<NavLink
+									end
+									to='/j4y'
+									className={({ isActive }) =>
+										classNames("p-2", {
+											"border-b-2 border-b-[#24459A]": isActive,
+										})
+									}
+								>
+									<HomeIcon />
+								</NavLink>
 
-                                <Link to='/j4y/my-network' className="p-2">
-                                    <GroupsIcon number={pendingRequests?.length > 0 && pendingRequests.length}/>
-                                </Link>
+								<NavLink
+									to='/j4y/my-network'
+									className={({ isActive }) =>
+										classNames("p-2", {
+											"border-b-2 border-b-[#24459A]": isActive,
+										})
+									}
+								>
+									<GroupsIcon
+										number={
+											pendingRequests?.length > 0 && pendingRequests.length
+										}
+									/>
+								</NavLink>
 
-                                <Link to='/j4y' className="p-2">
-                                    <JobsIcon/>
-                                </Link>
+								<NavLink
+									to='/j4y/jobs'
+									className={({ isActive }) =>
+										classNames("p-2", {
+											"border-b-2 border-b-[#24459A]": isActive,
+										})
+									}
+								>
+									<JobsIcon />
+								</NavLink>
 
-                                <Link to='/j4y/chats' className="p-2">
-                                    <MessagesIcon number={unReadMessages || null}/>
-                                </Link>
+								<NavLink
+									to='/j4y/chats'
+									className={({ isActive }) =>
+										classNames("p-2", {
+											"border-b-2 border-b-[#24459A]": isActive,
+										})
+									}
+								>
+									<MessagesIcon number={unReadMessages || null} />
+								</NavLink>
 
-                                <Link to='/j4y' className="p-2">
-                                    <BellIcon/>
-                                </Link>
-                            </div>
+								<NavLink
+									to='/j4y/notifications'
+									className={({ isActive }) =>
+										classNames("p-2", {
+											"border-b-2 border-b-[#24459A]": isActive,
+										})
+									}
+								>
+									<BellIcon />
+								</NavLink>
+							</div>
 
-                            <div className="flex flex-row ml-10">
-                                <CompanyButton/>
+							<div className='flex flex-row ml-10'>
+								<CompanyButton />
 
-                                <AccountButton/>
-                            </div>
-                        </div>
-                    </header>
-                </Show.When>
+								<AccountButton />
+							</div>
+						</div>
+					</header>
+				</Show.When>
 
-                <Show.Else>
-                    <MobileHeader pendingRequests={pendingRequests} unReadMessages={unReadMessages}/>
-                </Show.Else>
-            </Show>
-        </HeaderProvider>
-    )
+				<Show.Else>
+					<MobileHeader
+						pendingRequests={pendingRequests}
+						unReadMessages={unReadMessages}
+					/>
+				</Show.Else>
+			</Show>
+		</HeaderProvider>
+	)
 }
-export default InHeader;
+export default InHeader
