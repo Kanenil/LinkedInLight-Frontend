382dd94ec70c6fd32d7fabdfa93fe26e03c8ec3d
Oleksandr Burda <oleksandrburda2004@gmail.com>
2024-05-02 21:47:44 +0300

    fix i18n

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,26 +1,24 @@
-import i18n from 'i18next';
-import {initReactI18next} from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import HttpBackend from "i18next-http-backend";
+import i18n from "i18next"
+import { initReactI18next } from "react-i18next"
+import LanguageDetector from "i18next-browser-languagedetector"
+import HttpBackend from "i18next-http-backend"
 
-const languages = ['en', 'uk'];
+const languages = ["en", "uk"]
 
 i18n
-    .use(HttpBackend)
-    .use(LanguageDetector)
-    .use(initReactI18next)
-    .init({
-        fallbackLng: 'uk',
-        debug: true,
-        whitelist: languages,
-        backend: {
-            loadPath: "/locales/{{lng}}/{{ns}}.json",
-        },
-        interpolation: {
-            escapeValue: false,
-        },
-        react: {
-            wait: true,
-            useSuspense: true
-        }
-    })
+	.use(HttpBackend)
+	.use(LanguageDetector)
+	.use(initReactI18next)
+	.init({
+		fallbackLng: "en",
+		whitelist: languages,
+		backend: {
+			loadPath: "/locales/{{lng}}/{{ns}}.json",
+		},
+		interpolation: {
+			escapeValue: false,
+		},
+		react: {
+			useSuspense: true,
+		},
+	})
