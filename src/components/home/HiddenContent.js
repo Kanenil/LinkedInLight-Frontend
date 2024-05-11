31d07aa1727eddbdff6ede120cf02201d54517ec
Oleksandr Burda <oleksandrburda2004@gmail.com>
2024-05-11 15:25:40 +0300

    Add translation for "Show more" button in home page

--- src/components/home/HiddenContent.js
+++ src/components/home/HiddenContent.js
@@ -1,12 +1,14 @@
-import ArrowDownIcon from "../../elements/icons/ArrowDownIcon";
+import { useTranslation } from "react-i18next"
+import ArrowDownIcon from "../../elements/icons/ArrowDownIcon"
 
-const HiddenContent = (props) => {
-  return (
-    <div className="w-72 text-white text-xl bg-[#0A48DBB2] block text-center px-14 py-2 rounded-3xl mx-1 my-2 hover:bg-blue-700 transition duration-500 ease-in-out">
-      <span className="pr-5">Show More</span>{" "}
-      <ArrowDownIcon className="inline-block fill-white w-6 h-6" />
-    </div>
-  );
-};
+const HiddenContent = props => {
+	const { t } = useTranslation()
+	return (
+		<div className='w-72 cursor-pointer text-white text-xl bg-[#0A48DBB2] block text-center px-14 py-2 rounded-3xl mx-1 my-2 hover:bg-blue-700 transition duration-500 ease-in-out'>
+			<span className='pr-5'>{t("home.showMore")}</span>{" "}
+			<ArrowDownIcon className='inline-block fill-white w-6 h-6' />
+		</div>
+	)
+}
 
-export default HiddenContent;
+export default HiddenContent
